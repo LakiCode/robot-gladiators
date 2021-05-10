ef8ad6e26241e0af6a770c9f354e63f6e53dc702
rfuke <rfuke@loffacorp.com>
2021-05-10 00:10:08 -0700

    add variable to js game page

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert ("This is an alert! JavaSript is running!")
+// this was my first java code
+// window.alert ("This is an alert! JavaSript is running!")
+// now is time to move to something more intresting! ;-)
+
+// Promt for robot name
+// window.prompt ("What is your robot's name?");
+
+//use same script from above and create varible
+var playerName = window.prompt ("What is your robot's name?");
+//window.alert (playerName);
+// use cosole log to log input
+console.log ("This logs a string, good as testing to leave message to your self");
+//this can do math
+console.log(10+20);
+// This will write robot's name in sonsole
+console.log("My robot's name is " + playerName);
+
+//this create a fuction named "fight"
+function fight() {
+    window.alert ("The fight has begun!");
+}
+
+// fight();
